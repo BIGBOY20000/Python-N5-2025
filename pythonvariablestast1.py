1cca1d8217a8cccc9ee05cd78bc533458ee84974
Kevin Watt <K.Watt@albynschool.org>
2025-06-16 09:53:12 +0100

    KIJI

---
+++ pythonvariablestast1.py
@@ -0,0 +1,9 @@
+#task 1 :
+
+name = "kevin"
+age = 15
+favourite_subject = "comp science"
+
+print("what is your name", name)
+print("how old are you", age)
+print("favourite_subject", favourite_subject)
